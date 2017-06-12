f0fab7b654c828c2196929f60a4de5f4a5ab89ad
Egemen Ayhan <eayhan3469@gmail.com>
2017-06-13 01:37:22 +0300

    home.php sayfasina menu eklendi icerik düzenlenmedi

--- home.php
+++ home.php
@@ -2,8 +2,8 @@
 
 	session_start();
 	if(isset($_SESSION['user_id'])){
-		$user_id = $_SESSION['user_id'];
 		
+		$user_id = $_SESSION['user_id'];
 		$connection = mysqli_connect('localhost','root','root','kiosk');
 		$data = mysqli_query($connection, "SELECT * FROM users WHERE id = '$user_id'");
 		
@@ -16,6 +16,7 @@
 		}
 		else
 		{
+			session_destroy();
 			header("Location: Index.php");
 			exit;
 		}
@@ -31,17 +32,40 @@
 	<body>
 		<div id="logoDiv"><img src="rtalogo.png"></div><br>
 		<div id="container">
-			<div id="menuBar">
-				Welcome,<?php echo $first_name; ?>
-				<?php echo $last_name; ?><br><br>
-				<button>Add User</button>
-				<button>Add Announcements</button>
-				<button></button><a href="logout.php">Logout</a>
-				<button>Add User</button>
+			<div id="menuBar" class="tab">
+				Welcome,&nbsp;<?php echo $first_name; ?>
+					<br><br>
+				<button class="tablinks" id="defaultOpen" onclick="openPage(event,'addUser')">Add User</button>
+				<button class="tablinks" onclick="openPage(event,'addAnnouncement')">Add Announcements</button>
+				<button class="tablinks" onclick="location.href='logout.php'">Logout</button>
 			</div>
 			<div id="contentBar">
+				<div id="addUser" class="tabcontent">
+					Add User
+				</div>
+				
+				<div id="addAnnouncement" class="tabcontent">
+					Add Announcement
+				</div>
 			</div>
 		</div>
+		<script>
+			function openPage(evt,pageName) {
+				var i, tabcontent, tablinks;
+				tabcontent = document.getElementsByClassName("tabcontent");
+					for(i = 0; i < tabcontent.length; i++){
+						tabcontent[i].style.display = "none";
+					}
+				tablinks = document.getElementsByClassName("tablinks");
+				for(i = 0; i < tablinks.length; i++)
+					{
+						tablinks[i].className = tablinks[i].className.replace(" active","");
+					}
+				document.getElementById(pageName).style.display = "block";
+				evt.currentTarget.className += " active";
+			}
+			document.getElementById("defaultOpen").click();
+		</script>
 		
 	</body>
 </html>
